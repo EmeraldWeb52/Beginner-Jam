303601f524f0301bf875a288771af984303707d1
JALBOD <benetone8@gmail.com>
2025-03-20 23:16:54 +0200

    Great

---
+++ Beginner Jam/Assets/FortniteCharacter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class FortniteCharacter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         var keyboard = Keyboard.current;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
