91b7c9b53ff266f741780ffdbf3cf265042c4735
JALBOD <benetone8@gmail.com>
2025-03-20 23:19:20 +0200

    useful

--- Beginner Jam/Assets/Character.cs
+++ Beginner Jam/Assets/Character.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class FortniteCharacter : MonoBehaviour
+public class Character : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
